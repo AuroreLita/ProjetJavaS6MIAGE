729f6a46a4dbc04add3b41c6f1109bffeb32ebbb
AuroreLita <aurore.queille@free.fr>
2015-03-10 09:17:51 +0100

    Create Avion.java

---
+++ Avion.java
@@ -0,0 +1,16 @@
+import java.util.Hashtable;
+
+
+public class Avion {
+	private String Immatriculation;
+	private String typeAvion;
+	private Hashtable lesParking = new Hashtable(); /* clé : NomParking */
+	
+	public Avion(String immat, String type)
+	{
+		Immatriculation=immat;
+		typeAvion=type;
+	}
+	
+	// ajouter un parking
+}
