39fc607b679d2fab3b21617ee59561c19d28cdcb
AuroreLita <aurore.queille@free.fr>
2015-03-10 09:20:58 +0100

    Create Porte.java

---
+++ Porte.java
@@ -0,0 +1,14 @@
+
+public class Porte {
+	private String nomPorte;
+	private PointParking parking;
+	private Hall hall;
+	
+	public Porte(String n, PointParking p)
+	{
+		nomPorte=n;
+		parking=p;
+	}
+	
+	// ajouter à un hall
+}
