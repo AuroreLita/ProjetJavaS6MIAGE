197663060394cb833e1a71a67b7d6af348a8376f
AuroreLita <aurore.queille@free.fr>
2015-03-10 09:18:43 +0100

    Create Hall.java

---
+++ Hall.java
@@ -0,0 +1,14 @@
+import java.util.Hashtable;
+
+
+public class Hall {
+	private int numHall;
+	private Hashtable lesPortes = new Hashtable(); /* clé : NomPorte */
+	
+	public Hall(int n)
+	{
+		numHall=n;
+	}
+	
+	// ajouter une porte
+}
